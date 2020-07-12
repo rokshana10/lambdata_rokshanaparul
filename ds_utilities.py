96c402895000165194dace00e0365217ecdf617a
Rokshana Parul <rokshana10@yahoo.com>
2020-07-11 23:53:28 -0700

    Inatialize files in repo

---
+++ ds_utilities.py
@@ -0,0 +1,6 @@
+def enlarge(n):
+    ''' This function will '''
+    return n * 100
+
+y = int(input("Choose a number="))
+print(y, enlarge(y))
---
+++ my_lambdata/ds_utilities.py
@@ -0,0 +1,7 @@
+import pandas as pd
+def enlarge(n):
+    ''' This function will '''
+    return n * 100
+
+y = int(input("Choose a number="))
+print(y, enlarge(y))
