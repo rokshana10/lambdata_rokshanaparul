555e839253e542fa2554272555ebe083155e64fb
Rokshana Parul <rokshana10@yahoo.com>
2020-07-13 18:30:36 -0700

    my label goes here

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata_rokshanaparul", # Replace with your own username
-    version="0.0.1",
+    version="0.0.3",
     author="Rokshana Parul",
     author_email="author@example.com",
     description="A small example package",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
